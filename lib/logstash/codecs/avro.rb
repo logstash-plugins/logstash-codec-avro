9e9ed08be632d40d0a92ed4943c4b030d5be4313
Suyog Rao <suyog.rao@gmail.com>
2015-08-03 20:44:43 -0700

    Update avro.rb

--- lib/logstash/codecs/avro.rb
+++ lib/logstash/codecs/avro.rb
@@ -12,20 +12,20 @@ require "logstash/util"
 # Avro datums, as well as deserializing Avro datums into 
 # Logstash events.
 #
-# === Encoding
+# ==== Encoding
 # 
 # This codec is for serializing individual Logstash events 
 # as Avro datums that are Avro binary blobs. It does not encode 
 # Logstash events into an Avro file.
 #
 #
-# === Decoding
+# ==== Decoding
 #
 # This codec is for deserializing individual Avro records. It is not for reading
 # Avro files. Avro files have a unique format that must be handled upon input.
 #
 #
-# === Usage
+# ==== Usage
 # Example usage with Kafka input.
 #
 # [source,ruby]
@@ -50,11 +50,12 @@ class LogStash::Codecs::Avro < LogStash::Codecs::Base
   config_name "avro"
 
 
-  # schema path to fetch the schema from
+  # schema path to fetch the schema from.
   # This can be a 'http' or 'file' scheme URI
   # example:
-  #     http - "http://example.com/schema.avsc"
-  #     file - "/path/to/schema.avsc"
+  #
+  # * http - `http://example.com/schema.avsc`
+  # * file - `/path/to/schema.avsc`
   config :schema_uri, :validate => :string, :required => true
 
   def open_and_read(uri_string)
